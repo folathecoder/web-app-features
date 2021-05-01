8da909165185d43998281f659d7ccf420a1f00c6
folathecoder <officialakinloyefolarin@gmail.com>
2021-05-01 23:05:46 +0100

    Compeleted Tabbed Content

--- app.js
+++ app.js
@@ -6,19 +6,29 @@ const panels = document.querySelectorAll('.panel');
 
 //Add a click event listener to the whole tab and listen for "li" tags
 
-tabs.addEventListener('click', function(e){
+tabs.addEventListener('click', function (e) {
     //Search For an LI tag: Is it an Li?
-    if(e.target.tagName == 'LI') {
+    if (e.target.tagName == 'LI') {
+
         // If a tab is clicked, make it the target panel
         const targetPanel = document.querySelector(e.target.dataset.target);
 
         // Insert active into the clicked tab and remove it from others
-        panels.forEach(function(panel){
-            if (panel == targetPanel){
+        panels.forEach(function (panel) {
+            if (panel == targetPanel) {
                 panel.classList.add('active');
             }
             else {
-                panel.classList.remove('active');  
+                panel.classList.remove('active');
+            }
+        })
+
+        tabs.forEach(function (tab) {
+            if (tab == targetPanel) {
+                tab.classList.add('active');
+            }
+            else {
+                tab.classList.remove('active');
             }
         })
     }
